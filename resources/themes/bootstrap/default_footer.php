0ab52fd0a92906589bf1a18edd52e007fcd26523
Nobiel <huangzshi@gmail.com>
2017-06-10 21:57:54 +0800

    update footer

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
     本站托管于<a href="https://manage.hostdare.com/aff.php?aff=346" target="_blank"> Hostdare</a><br>
-    <a href="https://doub.io/dbrj-3/" target="_blank">采用逗比魔改版程序</a> / <a href="https://github.com/ToyoDAdoubi/DirectoryLister" target="_blank">Github开源</a> · 基于 <a href="http://www.directorylister.com/" target="_blank">Directory Lister</a> © 2015-<?php echo date("Y")?>
+    采用<a href="https://doub.io/dbrj-3/" target="_blank">逗比魔改版程序</a> / <a href="https://github.com/ToyoDAdoubi/DirectoryLister" target="_blank">Github开源</a> · 基于 <a href="http://www.directorylister.com/" target="_blank">Directory Lister</a> © 2015-<?php echo date("Y")?>
 </div>
